9676db37d585bf8e3465a10ff8ee0626029f4e15
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-03 16:05:17 +0100

    children

--- DOM/domNavigation.js
+++ DOM/domNavigation.js
@@ -1,3 +1,17 @@
 //body
 const body = document.body; // Обращаемся к body
 console.log(body);
+
+
+//children
+
+const children = document.body.childNodes; // childNodes хранит все дочернии элементы, включая текст
+
+let firstChild = children[0];
+
+firstChild = document.body.firstChild; // Обращение к первому элементу
+
+let lastChild = document.body.lastChild; // Обращение к последнему элементу
+console.log('children = ', children);
+console.log('firstChild = ', firstChild);
+console.log('lastChild = ', lastChild);
