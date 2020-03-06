47728945593625f41051425c949c254645f9f5f8
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-06 23:53:13 +0100

    closest

--- DOM/domNavigation.js
+++ DOM/domNavigation.js
@@ -7,7 +7,7 @@ console.log('body = ', body);
 
 const children = document.body.childNodes; // childNodes хранит все дочернии элементы, включая текст
 
-let firstChild = children[0];
+firstChild = children[0];
 
 firstChild = document.body.firstChild; // Обращение к первому элементу
 
@@ -15,3 +15,11 @@ let lastChild = document.body.lastChild; // Обращение к последн
 console.log('children = ', children);
 console.log('firstChild = ', firstChild);
 console.log('lastChild = ', lastChild);
+
+//closest
+let title1 = document.getElementById('title');
+
+const wrap = title1.closest('.wrap'); // Ищет выше по ирархии указанный элемент
+console.log(title1);
+console.log(wrap);
+
