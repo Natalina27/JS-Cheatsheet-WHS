5d78e5e7d7d21382208ed21e6dd5d60c5ef5584b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-17 00:04:15 +0100

    array methods: pop/push shift/unshift

---
+++ array.js
@@ -0,0 +1,15 @@
+const dish = ['pasta', 'risotto', 'polenta'];
+console.log(dish);
+
+dish.pop(); // Команда pop удаляет последний элемент из массива.
+console.log('pop' , dish);
+
+dish.push('pen'); // Добавляет элемент в конец массива.
+console.log('push' , dish);
+
+
+dish.shift(); // Команда shift удаляет первый элемент из массива.
+console.log('shift' , dish);
+
+dish.unshift('car'); // Добавляет элемент к начало массива.
+console.log('unshift' , dish);
