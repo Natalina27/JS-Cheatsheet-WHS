2324f509fdb15615919a12dce9cd6206452ab64c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-18 01:24:57 +0100

    array filter method

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-const dish = ['pasta', 'risotto', 'polenta'];
+const dish = ['pasta', 'risotto', 'polenta','tomato soup','tomato soup','tomato soup'];
 console.log(dish);
 
 dish.pop(); // Команда pop удаляет последний элемент из массива.
@@ -13,3 +13,6 @@ console.log('shift' , dish);
 
 dish.unshift('car'); // Добавляет элемент к начало массива.
 console.log('unshift' , dish);
+
+const  favouriteDish = dish.filter(el => el === 'tomato soup'); // условие, которое должно выполниться, чтобы елемент попал в новый массив
+console.log(favouriteDish);
