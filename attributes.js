fa92b5b0fbc43802708e79f0a7acb4c59eecf08b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-02 12:18:40 +0200

    hasAttribute

--- attributes.js
+++ attributes.js
@@ -1,2 +1,5 @@
 const title = document.getElementById('title');
 console.log('title for attributes: ', title);
+
+let nameAttribute = title.hasAttribute('name'); // Проверяет наличие атрибута
+console.log('hasAttribute name ? :' , nameAttribute);
