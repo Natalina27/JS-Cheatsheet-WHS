043d49d81ddc32a0d73b7322f67d138b3ff8f424
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-08 12:58:25 +0200

    setAttribute

--- attributes.js
+++ attributes.js
@@ -1,8 +1,11 @@
 const title = document.getElementById('title');
 console.log('title for attributes: ', title);
 
-let attrName = title.hasAttribute('name'); // Проверяет наличие атрибута
-console.log('hasAttribute name ? :' , attrName);
+title.setAttribute('name', 'new-name-title'); // Устанавливает новое значение атрибута
 
 let attrValue = title.getAttribute('name'); // Получает значение атрибута
 console.log('getAttribute name = ' , attrValue);
+
+let attrName = title.hasAttribute('name'); // Проверяет наличие атрибута
+console.log('hasAttribute name ? :' , attrName);
+
