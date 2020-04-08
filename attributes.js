caa8a1861db7bd7070df2dee8f75104926e59089
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-08 13:00:40 +0200

    removeAttribute

--- attributes.js
+++ attributes.js
@@ -1,11 +1,26 @@
 const title = document.getElementById('title');
 console.log('title for attributes: ', title);
 
-title.setAttribute('name', 'new-name-title'); // Устанавливает новое значение атрибута
-
 let attrValue = title.getAttribute('name'); // Получает значение атрибута
 console.log('getAttribute name = ' , attrValue);
 
 let attrName = title.hasAttribute('name'); // Проверяет наличие атрибута
 console.log('hasAttribute name ? :' , attrName);
+
+title.setAttribute('name', 'new-name-title'); // Устанавливает новое значение атрибута
+
+ attrValue = title.getAttribute('name'); // Получает значение атрибута
+console.log('getAttribute name = ' , attrValue);
+
+ attrName = title.hasAttribute('name'); // Проверяет наличие атрибута
+console.log('hasAttribute name ? :' , attrName);
+
+title.removeAttribute('name'); // Удаляет атрибут
+
+ attrValue = title.getAttribute('name'); // Получает значение атрибута
+console.log('getAttribute name = ' , attrValue);
+
+ attrName = title.hasAttribute('name'); // Проверяет наличие атрибута
+console.log('hasAttribute name ? :' , attrName);
+
 
